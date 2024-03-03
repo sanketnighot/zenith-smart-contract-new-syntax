e251685653ca90c3c5504494ad4bb47230105ef9
sanketnighot <sanketnighot25@gmail.com>
2024-03-04 01:22:49 +0530

    Updated Orders Smart Contract

--- vmm_orders.py
+++ vmm_orders.py
@@ -45,7 +45,7 @@ def orders():
         @sp.private(with_storage="read-only")
         def _checkStatus(self, statusCode):
             sp.cast(statusCode, sp.int)
-            assert self.data.status == statusCode, "InvalidStatus"
+            assert self.data.status == statusCode, "InvalidContractStatus"
 
         # Call Get Mark and Index Price from VMM Contract View
         @sp.private(with_operations=True)
@@ -56,7 +56,7 @@ def orders():
                 vmm_address,
                 (),
                 sp.record(index_price=sp.int, mark_price=sp.int),
-            ).unwrap_some()
+            ).unwrap_some(error="ErrorInCallGetIndexAndMarkPriceView")
             return view_data
 
         # Call Get Position Data from VMM Contract View
@@ -69,7 +69,7 @@ def orders():
                 vmm_address,
                 position_holder,
                 vmm_types.positions_value,
-            ).unwrap_some()
+            ).unwrap_some(error="ErrorInCallGetPositionDataView")
             return view_data
 
         # Call Increase Position from VMM Contract
@@ -94,7 +94,7 @@ def orders():
                 ),
                 params.vmm_address,
                 "increasePosition",
-            ).unwrap_some()
+            ).unwrap_some(error="ErrorInCallIncreasePosition")
 
             dataToBeSent = sp.record(
                 position_holder=params.position_holder,
@@ -113,7 +113,7 @@ def orders():
             )
             contractParams = sp.contract(
                 sp.address, params.vmm_address, "closePosition"
-            ).unwrap_some()
+            ).unwrap_some(error="ErrorInCallClosePosition")
             sp.transfer(params.position_holder, sp.mutez(0), contractParams)
 
         #  Call Decrease Position from VMM Contract
@@ -126,6 +126,7 @@ def orders():
                     position_holder=sp.address,
                     direction=sp.int,
                     usd_amount=sp.int,
+                    leverage_multiple=sp.int,
                 ),
             )
             contractParams = sp.contract(
@@ -136,7 +137,7 @@ def orders():
                 ),
                 params.vmm_address,
                 "decreasePosition",
-            ).unwrap_some()
+            ).unwrap_some(error="ErrorInCallDecreasePosition")
 
             dataToBeSent = sp.record(
                 position_holder=params.position_holder,
@@ -161,7 +162,7 @@ def orders():
                 sp.record(position_holder=sp.address, amount=sp.int),
                 params.vmm_address,
                 "addMargin",
-            ).unwrap_some()
+            ).unwrap_some(error="ErrorInCallAddMargin")
 
             dataToBeSent = sp.record(
                 position_holder=params.position_holder,
@@ -185,7 +186,7 @@ def orders():
                 sp.record(position_holder=sp.address, amount=sp.int),
                 params.vmm_address,
                 "removeMargin",
-            ).unwrap_some()
+            ).unwrap_some(error="ErrorInCallRemoveMargin")
 
             dataToBeSent = sp.record(
                 position_holder=params.position_holder,
@@ -206,7 +207,7 @@ def orders():
             )
             contractParams = sp.contract(
                 sp.address, params.vmm_address, "takeProfit"
-            ).unwrap_some()
+            ).unwrap_some(error="ErrorInCallTakeProfit")
             sp.transfer(params.position_holder, sp.mutez(0), contractParams)
 
         # Update Admin
@@ -226,10 +227,14 @@ def orders():
             ), "NoPendingAdministrator"
             assert (
                 sp.sender
-                == self.data.administration_panel.pendingAdministrator.unwrap_some()
+                == self.data.administration_panel.pendingAdministrator.unwrap_some(
+                    error="ErrorInPendingAdministrator"
+                )
             ), "NotAuthorized"
             self.data.administration_panel.administrator = (
-                self.data.administration_panel.pendingAdministrator.unwrap_some()
+                self.data.administration_panel.pendingAdministrator.unwrap_some(
+                    error="ErrorInPendingAdministrator"
+                )
             )
             self.data.administration_panel.pendingAdministrator = None
 
@@ -270,10 +275,10 @@ def orders():
         @sp.entrypoint
         def createOrder(self, params):
             sp.cast(params, vmm_types.create_order_type)
-            assert self.data.status == 1, "Contract is not active"
+            self._checkStatus(1)
             assert params.position_holder == sp.sender, "InvalidPositionHolder"
             self.data.orders[self.data.last_order_id] = params
-            if (params.order_type == 0) and (params.trigger_price == 0):
+            if params.order_type == 0:
                 order_params = sp.record(
                     vmm_address=params.vmm_address,
                     position_holder=params.position_holder,
@@ -285,26 +290,204 @@ def orders():
                 self.data.orders[self.data.last_order_id].order_status = 1
             self.data.last_order_id += 1
 
-        # Update Order
+        # Update Pending Order
         @sp.entrypoint
-        def updateOrder(self, order_id, params):
+        def updatePendingOrder(self, order_id, params):
             sp.cast(params, vmm_types.create_order_type)
-            assert self.data.status == 1, "Contract is not active"
-            assert self.data.orders.contains(order_id), "Order does not exist"
+            self._checkStatus(1)
+            assert self.data.orders.contains(order_id), "InvalidOrderId"
+            assert (
+                self.data.orders[order_id].position_holder == sp.sender
+            ), "NotAuthorized"
+            assert self.data.orders[order_id].order_status == 0, "InvalidOrderStatus"
             self.data.orders[order_id] = params
+
+        # Update Active Order
+        @sp.entrypoint
+        def increaseActiveOrder(self, params):
+            sp.cast(
+                params,
+                sp.record(
+                    order_id=sp.int,
+                    amount_in=sp.int,
+                    leverage_multiple=sp.int,
+                    stop_trigger_price=sp.option[sp.int],
+                    stop_limit_price=sp.option[sp.int],
+                    take_trigger_price=sp.option[sp.int],
+                    take_limit_price=sp.option[sp.int],
+                    expiration=sp.int,
+                ),
+            )
+            self._checkStatus(1)
+            assert self.data.orders.contains(params.order_id), "InvalidOrderId"
+            assert (
+                self.data.orders[params.order_id].position_holder == sp.sender
+            ), "NotAuthorized"
+            assert (
+                self.data.orders[params.order_id].order_status == 1
+            ), "InvalidOrderStatus"
+            self.data.orders[params.order_id].amount_in += params.amount_in
+            self.data.orders[params.order_id].leverage_multiple = (
+                params.leverage_multiple
+            )
+            self.data.orders[params.order_id].stop_trigger_price = (
+                params.stop_trigger_price
+            )
+            self.data.orders[params.order_id].stop_limit_price = params.stop_limit_price
+            self.data.orders[params.order_id].take_trigger_price = (
+                params.take_trigger_price
+            )
+            self.data.orders[params.order_id].take_limit_price = params.take_limit_price
+            self.data.orders[params.order_id].expiration = params.expiration
+
+            order_params = sp.record(
+                vmm_address=self.data.orders[params.order_id].vmm_address,
+                position_holder=self.data.orders[params.order_id].position_holder,
+                direction=self.data.orders[params.order_id].direction,
+                usd_amount=params.amount_in,
+                leverage_multiple=params.leverage_multiple,
+            )
+            self.callIncreasePosition(order_params)
+
+        # Decrease Active Order
+        @sp.entrypoint
+        def decreaseActiveOrder(self, params):
+            sp.cast(
+                params,
+                sp.record(
+                    order_id=sp.int,
+                    amount_in=sp.int,
+                    leverage_multiple=sp.int,
+                    stop_trigger_price=sp.option[sp.int],
+                    stop_limit_price=sp.option[sp.int],
+                    take_trigger_price=sp.option[sp.int],
+                    take_limit_price=sp.option[sp.int],
+                    expiration=sp.int,
+                ),
+            )
+            self._checkStatus(1)
+            assert self.data.orders.contains(params.order_id), "InvalidOrderId"
+            assert (
+                self.data.orders[params.order_id].position_holder == sp.sender
+            ), "NotAuthorized"
+            assert (
+                self.data.orders[params.order_id].order_status == 1
+            ), "InvalidOrderStatus"
+            assert (
+                self.data.orders[params.order_id].amount_in >= params.amount_in
+            ), "InvalidAmount"
+            self.data.orders[params.order_id].amount_in -= params.amount_in
+            self.data.orders[params.order_id].leverage_multiple = (
+                params.leverage_multiple
+            )
+            self.data.orders[params.order_id].stop_trigger_price = (
+                params.stop_trigger_price
+            )
+            self.data.orders[params.order_id].stop_limit_price = params.stop_limit_price
+            self.data.orders[params.order_id].take_trigger_price = (
+                params.take_trigger_price
+            )
+            self.data.orders[params.order_id].take_limit_price = params.take_limit_price
+            self.data.orders[params.order_id].expiration = params.expiration
+            order_params = sp.record(
+                vmm_address=self.data.orders[params.order_id].vmm_address,
+                position_holder=self.data.orders[params.order_id].position_holder,
+                direction=self.data.orders[params.order_id].direction,
+                usd_amount=params.amount_in,
+                leverage_multiple=params.leverage_multiple,
+            )
+            self.callDecreasePosition(order_params)
+
+        # Execute Add Margin
+        @sp.entrypoint
+        def executeAddMargin(self, params):
+            sp.cast(
+                params,
+                sp.record(
+                    order_id=sp.int,
+                    amount=sp.int,
+                    stop_trigger_price=sp.option[sp.int],
+                    stop_limit_price=sp.option[sp.int],
+                    take_trigger_price=sp.option[sp.int],
+                    take_limit_price=sp.option[sp.int],
+                    expiration=sp.int,
+                ),
+            )
+            self._checkStatus(1)
+            assert self.data.orders.contains(params.order_id), "InvalidOrderId"
+            assert (
+                self.data.orders[params.order_id].position_holder == sp.sender
+            ), "NotAuthorized"
+            assert (
+                self.data.orders[params.order_id].order_status == 1
+            ), "InvalidOrderStatus"
+            self.data.orders[params.order_id].stop_trigger_price = (
+                params.stop_trigger_price
+            )
+            self.data.orders[params.order_id].stop_limit_price = params.stop_limit_price
+            self.data.orders[params.order_id].take_trigger_price = (
+                params.take_trigger_price
+            )
+            self.data.orders[params.order_id].take_limit_price = params.take_limit_price
+            self.data.orders[params.order_id].expiration = params.expiration
+            order_params = sp.record(
+                vmm_address=self.data.orders[params.order_id].vmm_address,
+                position_holder=self.data.orders[params.order_id].position_holder,
+                amount=params.amount,
+            )
+            self.callAddMargin(order_params)
+
+        # Execute Remove Margin
+        @sp.entrypoint
+        def executeRemoveMargin(self, params):
+            sp.cast(
+                params,
+                sp.record(
+                    order_id=sp.int,
+                    amount=sp.int,
+                    stop_trigger_price=sp.option[sp.int],
+                    stop_limit_price=sp.option[sp.int],
+                    take_trigger_price=sp.option[sp.int],
+                    take_limit_price=sp.option[sp.int],
+                    expiration=sp.int,
+                ),
+            )
+            self._checkStatus(1)
+            assert self.data.orders.contains(params.order_id), "InvalidOrderId"
+            assert (
+                self.data.orders[params.order_id].position_holder == sp.sender
+            ), "NotAuthorized"
+            assert (
+                self.data.orders[params.order_id].order_status == 1
+            ), "InvalidOrderStatus"
+            self.data.orders[params.order_id].stop_trigger_price = (
+                params.stop_trigger_price
+            )
+            self.data.orders[params.order_id].stop_limit_price = params.stop_limit_price
+            self.data.orders[params.order_id].take_trigger_price = (
+                params.take_trigger_price
+            )
+            self.data.orders[params.order_id].take_limit_price = params.take_limit_price
+            self.data.orders[params.order_id].expiration = params.expiration
+            order_params = sp.record(
+                vmm_address=self.data.orders[params.order_id].vmm_address,
+                position_holder=self.data.orders[params.order_id].position_holder,
+                amount=params.amount,
+            )
+            self.callRemoveMargin(order_params)
 
         # Cancel Order
         @sp.entrypoint
         def cancelOrder(self, order_id):
-            assert self.data.status == 1, "Contract is not active"
-            assert self.data.orders.contains(order_id), "Order does not exist"
+            self._checkStatus(1)
+            assert self.data.orders.contains(order_id), "InvalidOrderId"
             del self.data.orders[order_id]
 
         # Execute Limit Order
         @sp.entrypoint
         def executeLimitOrder(self, order_id):
-            assert self.data.status == 1, "Contract is not active"
-            assert self.data.orders.contains(order_id), "Order does not exist"
+            self._checkStatus(1)
+            assert self.data.orders.contains(order_id), "InvalidOrderId"
             # TODO: Check the Trigger Price and Current Mark Price are in range of execution
             current_index_and_mark_price = self.callGetIndexAndMarkPriceView(
                 self.data.orders[order_id].vmm_address
@@ -339,12 +522,12 @@ def orders():
         # Execute Close Position
         @sp.entrypoint
         def executeCloseOrder(self, order_id):
-            assert self.data.status == 1, "Contract is not active"
-            assert self.data.orders.contains(order_id), "Order does not exist"
+            self._checkStatus(1)
+            assert self.data.orders.contains(order_id), "InvalidOrderId"
             assert (
                 sp.sender == self.data.orders[order_id].position_holder
             ), "NotAuthorized"
-            assert self.data.orders[order_id].order_status == 1, "Order is not active"
+            assert self.data.orders[order_id].order_status == 1, "InvalidOrderStatus"
             order_params = sp.record(
                 vmm_address=self.data.orders[order_id].vmm_address,
                 position_holder=self.data.orders[order_id].position_holder,
@@ -355,14 +538,16 @@ def orders():
         # Trigger Stop Loss
         @sp.entrypoint
         def triggerStopLoss(self, order_id):
-            assert self.data.status == 1, "Contract is not active"
-            assert self.data.orders.contains(order_id), "Order does not exist"
+            self._checkStatus(1)
+            assert self.data.orders.contains(order_id), "InvalidOrderId"
             current_index_and_mark_price = self.callGetIndexAndMarkPriceView(
                 self.data.orders[order_id].vmm_address
             )
             if (self.data.orders[order_id].direction == 1) and (
                 current_index_and_mark_price.mark_price
-                <= self.data.orders[order_id].stop_trigger_price.unwrap_some()
+                <= self.data.orders[order_id].stop_trigger_price.unwrap_some(
+                    error="ErrorInStopTriggerPrice"
+                )
             ):
                 order_params = sp.record(
                     vmm_address=self.data.orders[order_id].vmm_address,
@@ -372,7 +557,9 @@ def orders():
                 self.data.orders[order_id].order_status = 2
             if (self.data.orders[order_id].direction == 2) and (
                 current_index_and_mark_price.mark_price
-                >= self.data.orders[order_id].stop_trigger_price.unwrap_some()
+                >= self.data.orders[order_id].stop_trigger_price.unwrap_some(
+                    error="ErrorInStopTriggerPrice"
+                )
             ):
                 order_params = sp.record(
                     vmm_address=self.data.orders[order_id].vmm_address,
@@ -384,14 +571,16 @@ def orders():
         # Trigger Take Profit
         @sp.entrypoint
         def triggerTakeProfit(self, order_id):
-            assert self.data.status == 1, "Contract is not active"
-            assert self.data.orders.contains(order_id), "Order does not exist"
+            self._checkStatus(1)
+            assert self.data.orders.contains(order_id), "InvalidOrderId"
             current_index_and_mark_price = self.callGetIndexAndMarkPriceView(
                 self.data.orders[order_id].vmm_address
             )
             if (self.data.orders[order_id].direction == 1) and (
                 current_index_and_mark_price.mark_price
-                >= self.data.orders[order_id].take_trigger_price.unwrap_some()
+                >= self.data.orders[order_id].take_trigger_price.unwrap_some(
+                    error="ErrorInTakeTriggerPrice"
+                )
             ):
                 order_params = sp.record(
                     vmm_address=self.data.orders[order_id].vmm_address,
